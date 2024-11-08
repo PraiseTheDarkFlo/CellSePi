8f54723762903c3a116e08010358716b446566c8
Pasykaru <pascal.c.hess@outlook.de>
2024-11-08 15:52:15 +0100

    Atrtribute in CellSiPi

--- src/CellSePi.py
+++ src/CellSePi.py
@@ -1,4 +1,24 @@
 class CellSePi:
     def __init__(self):
         self.page = None
+        super().__init__()
+        self.segmentation_running = False
+        self.segmentation_thread = None
+        self.readout_running = False
+        self.readout_thread = None
+        self.readout_path = None
+
+        self.image_id = None
+        self.channel_id = None
+        self.image_view_update = True
+
+        self.readout = None
+
+        self.bit_depth = 16
+
+        self.images = []
+        self.image_paths = None
+        self.mask_paths = None
+        self.image_views = []
+
 
