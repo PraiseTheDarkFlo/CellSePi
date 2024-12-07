3f6d4822f6d4ae418153e9263b9aeab7e79fcc50
mmdark <fhock2002@gmail.com>
2024-12-07 02:56:41 +0100

    open PQT is working i guess

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -6,14 +6,13 @@ import flet as ft
 from PyQt5.QtWidgets import QApplication
 from scipy.constants import value
 
-from . import gui_options as op
+from . import gui_options as op, gui_segmentation
 from .drawing.gui_drawing import open_qt_window
 from .gui_canvas import Canvas
 from .gui_config import GUIConfig
 from .gui_directory import format_directory_path, copy_directory_to_clipboard, create_directory_card
 from src.CellSePi import CellSePi
 from src.mask import Mask
-from .gui_segmentation import gui_segmentation
 
 
 #class GUI to handle the complete GUI and their attributes, also contains the CellSePi class and updates their attributes
@@ -28,7 +27,7 @@ class GUI:
         self.tif_txt = ft.Text("Tif")
         self.is_lif = ft.CupertinoSwitch(value=True, active_color=ft.Colors.BLUE_ACCENT,track_color=ft.Colors.BLUE_ACCENT)
         self.switch_mask = ft.Switch(label="Mask", value=False)
-        self.drawing_button= ft.ElevatedButton(text="Drawing Tools", icon="brush_rounded",on_click=lambda e: multiprocessing.Process(target=open_qt_window).start())
+        self.drawing_button= ft.ElevatedButton(text="Drawing Tools", icon="brush_rounded",on_click=lambda e: multiprocessing.Process(target=open_qt_window,args=(self.csp,)).start())
         self.page.window.width = 1400
         self.page.window.height = 825
         self.page.window_left = 200
@@ -107,5 +106,4 @@ class GUI:
 
             self.page.update()
         self.switch_mask.on_change = update_view_mask
-
 
--- src/GUI/drawing/gui_drawing.py
+++ src/GUI/drawing/gui_drawing.py
@@ -1,11 +1,16 @@
 
 from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QPushButton, QWidget
 import sys
+from .. import GUI
+from ...CellSePi import CellSePi
+
 
 # PyQt-Fenster definieren
 class MyQtWindow(QWidget):
-    def __init__(self):
+    def __init__(self, csp: CellSePi):
         super().__init__()
+        self.csp = csp
+        print(csp.bit_depth)
         self.setWindowTitle("PyQt Window")
         self.setGeometry(100, 100, 400, 300)
 
@@ -19,8 +24,8 @@ class MyQtWindow(QWidget):
         self.setLayout(layout)
 
 # Funktion, um das PyQt-Fenster zu starten
-def open_qt_window():
+def open_qt_window(csp: CellSePi):
     app = QApplication(sys.argv)
-    window = MyQtWindow()
+    window = MyQtWindow(csp)
     window.show()
     app.exec()
