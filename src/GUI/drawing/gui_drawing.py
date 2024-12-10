f7faa69dc943a51536c5bab47e7bac4cbaef8240
Jenna <Jennaahlvers@t-online.de>
2024-12-10 15:42:43 +0100

    comments for the drawing window

--- src/GUI/drawing/gui_drawing.py
+++ src/GUI/drawing/gui_drawing.py
@@ -23,22 +23,13 @@ class MyQtWindow(QMainWindow):
         self.csp = csp
         print(csp.bit_depth)
         self.setWindowTitle("PyQt Window")
-        #self.resize(800,600)
+
         self.canvas= DrawingCanvas(self.csp)
         self.setCentralWidget(self.canvas)
 
         self.canvas.load_mask_to_scene()
         self.canvas.load_image_to_scene()
 
-        #window layout
-        #layout = QVBoxLayout()
-
-        #button = QPushButton("Close Window")
-        #button.clicked.connect(self.close)
-
-        #layout.addWidget(label)
-        #layout.addWidget(button)
-        #self.setLayout(layout)
 
     def resizeEvent(self, event):
         self.canvas.fitInView(self.canvas.sceneRect(), Qt.KeepAspectRatio)
@@ -55,6 +46,16 @@ def open_qt_window(csp: CellSePi):
 
 
 class DrawingCanvas(QGraphicsView):
+    """
+    In the class the images(background, mask) are loaded. Also the Drawing Canvas is implemented in here
+
+    Attributes:
+        csp=object of CellSePi
+        scene= main scene of the window, framework for images
+        mask= saves the mask of the cells
+        image_array= saves the datapoints of the cells
+        background_image= saves the main image of the cell
+    """
 
     def __init__(self, csp: CellSePi):
         super().__init__()
@@ -62,16 +63,23 @@ class DrawingCanvas(QGraphicsView):
         self.scene = QGraphicsScene(self)
 
         self.setScene(self.scene)
-        self.canvas = None
+        self.mask = None
         self.image_array=None
         self.background_image=None
 
 
     def load_mask_to_scene(self):
+        """
+        loads the mask of the main image as png into the scene and scales it to the
+        scene geometry
+
+        """
+
         current_path = pathlib.PosixPath
         if platform.system() == "Windows":
             pathlib.PosixPath = pathlib.WindowsPath
 
+        #loads the mask of the current image
         mask = np.load(self.csp.mask_paths[self.csp.image_id][self.csp.config.get_bf_channel()], allow_pickle=True).item()
 
         mask_data= mask["masks"]
@@ -84,15 +92,25 @@ class DrawingCanvas(QGraphicsView):
         self.image_array=image_mask
 
         pathlib.PosixPath = current_path
+
         #creat an image
         height,width,channel= self.image_array.shape
         image= QImage(self.image_array.data,width,height,QImage.Format_RGBA8888)
 
-        #set the image into canvas
+        #set the image into the mask
         pixmap = QPixmap.fromImage(image)
-        self.scale_image(pixmap,self.canvas)
+        self.scale_image(pixmap, self.mask)
 
     def scale_image(self,image,object,depth = 1):
+        """
+        does the scaling for the needed image and adds it to the window
+
+        Args:
+            image= the used image
+            object= the used object
+            depth= the depth of the image in the scene
+
+        """
         view_width = self.width()
         view_height = self.height()
 
@@ -104,12 +122,14 @@ class DrawingCanvas(QGraphicsView):
         object.setZValue(depth)
 
     def load_image_to_scene(self):
-
+        """
+        loads the main image into the scene
+        """
         if self.csp.adjusted_image_path is not None:
             image = QPixmap(self.csp.adjusted_image_path)
         else:
             image= QPixmap(self.csp.image_paths[self.csp.image_id])
-      #  image = QPixmap("my grandmother send her regards.jpg")
+
 
         self.scale_image(image,self.background_image,-1)
 
