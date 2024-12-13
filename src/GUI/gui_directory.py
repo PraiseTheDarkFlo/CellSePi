cfb7ac7d4fc7d34acbf697f75911232288dbe327
nikedratt <nikedratt@gmail.com>
2024-12-13 15:15:28 +0100

    tif error handling

--- src/GUI/gui_directory.py
+++ src/GUI/gui_directory.py
@@ -107,6 +107,10 @@ def create_directory_card(gui: GUI):
             os.rmdir(gui.csp.working_directory)
         elif not is_supported:
             gui.page.snack_bar = ft.SnackBar(ft.Text("The directory contains an unsupported file type. Only 8 or 16 bit .tiff files allowed."))
+            gui.count_results_txt.color = ft.Colors.RED
+            gui.page.update()
+            image_paths = {}
+            mask_paths = {}
         else:
             gui.count_results_txt.color = None
 
