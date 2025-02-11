acea27a6ab7bfd605a0573821eb6155c6253b109
mmdark <fhock2002@gmail.com>
2025-02-11 20:36:34 +0100

    added error when bright-field channel has no image when press draw button

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -179,12 +179,18 @@ class GUI:
         self.csp.window_bf_channel = self.csp.config.get_bf_channel()
         self.csp.window_channel_id = self.csp.channel_id
 
-        image_path = self.csp.image_paths[self.csp.image_id][self.csp.window_bf_channel]
-        directory, filename = os.path.split(image_path)
-        name, _ = os.path.splitext(filename)
-        mask_file_name = f"{name}{self.csp.config.get_mask_suffix()}.npy"
-        mask_path= os.path.join(directory, mask_file_name)
-        self.queue.put((self.csp.config.get_mask_color(), self.csp.config.get_outline_color(), self.csp.window_bf_channel, self.csp.mask_paths, self.csp.window_image_id, self.csp.adjusted_image_path, mask_path,self.csp.window_channel_id,self.csp.current_channel_prefix))
+        if self.csp.window_bf_channel in self.csp.image_paths[self.csp.image_id]:#check if the bf has an image
+            image_path = self.csp.image_paths[self.csp.image_id][self.csp.window_bf_channel]
+            directory, filename = os.path.split(image_path)
+            name, _ = os.path.splitext(filename)
+            mask_file_name = f"{name}{self.csp.config.get_mask_suffix()}.npy"
+            mask_path= os.path.join(directory, mask_file_name)
+            self.queue.put((self.csp.config.get_mask_color(), self.csp.config.get_outline_color(), self.csp.window_bf_channel, self.csp.mask_paths, self.csp.window_image_id, self.csp.adjusted_image_path, mask_path,self.csp.window_channel_id,self.csp.current_channel_prefix))
+        else:
+            self.page.snack_bar = ft.SnackBar(
+                ft.Text(f"Selected bright-field channel {self.csp.window_bf_channel},has no image!"))
+            self.page.snack_bar.open = True
+            self.page.update()
 
     def handle_closing_event(self, e):
         """
