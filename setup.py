ca9eb3d2f13616305cb3ab9210fa6d05dc1af589
mmdark <fhock2002@gmail.com>
2025-03-03 12:00:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="cellsepi",
-    version="0.1",
+    version="0.2",
     license="Apache License 2.0",
     packages=find_packages(where="src"),
     include_package_data=True,
