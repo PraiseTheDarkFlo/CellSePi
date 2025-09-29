ea5804ae0a88d4d8c428eb07cb1049e2439673b7
DarkFlo <fhock2002@gmail.com>
2025-09-30 01:14:20 +0200

    test

--- main.py
+++
@@ -1,7 +0,0 @@
-
-import flet as ft
-
-from cellsepi.__main__ import main
-
-if __name__ == "__main__":
-    ft.app(target=main)
--- src/cellsepi/main.py
+++ src/cellsepi/main.py
@@ -1,6 +1,12 @@
+import multiprocessing
+
 import flet as ft
 from cellsepi.frontend.main_window.gui import GUI
 
-async def main(page: ft.Page):
+def main():
+    multiprocessing.set_start_method("spawn")
+    ft.app(target=async_main, view=ft.FLET_APP)
+
+async def async_main(page: ft.Page):
     gui = GUI(page)
     gui.build()
